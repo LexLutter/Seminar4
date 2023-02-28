5f133be793dfb1bdd50f5208e3e00e754328adcc
LexLuter <gae_cbr@mail.ru>
2023-03-01 00:06:54 +0700

    task2_1

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,9 +1,15 @@
 ﻿Console.Write("Введите произвольное число: ");
 int FirstNumber = Convert.ToInt32(Console.ReadLine());
  int result = 0;
+ int a = FirstNumber / 10;
+
 while (FirstNumber / 10 > 0){
-   
+    result = FirstNumber % 10 + result;
     FirstNumber = FirstNumber / 10;
-    result++;
+     Console.WriteLine($"{FirstNumber % 10} ");
 }
-Console.WriteLine($"A в степени B равно: {result+1}.");
+if (FirstNumber % 10 != 0) {
+    result = FirstNumber % 10 + result;
+    Console.WriteLine($"{FirstNumber % 10} ");
+}
+Console.WriteLine($"A в степени B равно: {result}.");
