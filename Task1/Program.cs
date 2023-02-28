604d77bc1059aa5fcfb0669f83f89b48d7a4ff3b
LexLuter <gae_cbr@mail.ru>
2023-02-28 23:33:14 +0700

    task1

---
+++ Task1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число A: ");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+var result = Math.Pow(FirstNumber, SecondNumber);
+Console.WriteLine($"A в степени B равно: {result}.");
